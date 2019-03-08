f532b7fa8c0dd116f97a174f4ff1fb84bd54060a
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-03-08 11:47:47 -0500

    Primer Commit

---
+++ primos1.py
@@ -0,0 +1,13 @@
+print("comienzo")
+print()
+n1=int(input("Escriba un numero: "))
+a=("Es Divisible :D")
+for i in range(2,n1):
+   if n2%i==0:
+     a=("No es Divible :(")
+     break
+
+
+print(a)
+print()
+print("Fin")
